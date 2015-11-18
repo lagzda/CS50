be50e0cd33e8d405cd9c73bc1e206ee3790b2f1e
Ralfs Lagzda <lagzdar@uni.coventry.ac.uk>
2015-11-18 14:36:04 -0500

    Second

--- Set3/find/helpers.c
+++ Set3/find/helpers.c
@@ -8,22 +8,56 @@
  */
        
 #include <cs50.h>
-
 #include "helpers.h"
 
+//The first are searching algorithms! For comments see Algorithms folder.
+
+//Recursive Binary Search
+bool binary(int value, int values[], int min, int max){
+    if(min>max){
+        return false;
+    }
+    int midpoint = (min+max)/2;
+    if(values[midpoint] > value){
+        return binary(value,values,min,midpoint-1);
+    }
+    else if (values[midpoint] < value) {
+        return binary(value,values,midpoint+1,max);
+    } 
+    else {
+        return true;
+    }
+}
+
+//Simple linear search
+bool linear(int value, int values[], int size){
+    for (int i = 0; i < size; i++){
+        if (values[i] == value){
+            return true;
+        }
+    }
+    return false;
+}
 /**
  * Returns true if value is in array of n values, else false.
  */
 bool search(int value, int values[], int n)
-{
-    for (int i = 0; i < n; i++){
-        if(values[i]==value){
-            return true;
-        } 
-    }
-    return false;
+{   
+    //You can uncomment either linear or binary search to work
+    //return linear(value, values, n);
+    return binary(value, values, 0, n);
 }
 
+//Swap function for selection and bubble sort algorithms
+void swap(int * x, int * y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+//Sorting algorithms! Comments for these are in the Algorithms folder as well
+
+//Selection Sort
 void selection (int values[], int n){
      for (int i = 0; i<n; i++){
         int min = i;
@@ -33,12 +67,12 @@ void selection (int values[], int n){
             }
         }
         if (min!=i){
-            int temp = values[i];
-            values[i] = values[min];
-            values[min] = temp;
+            swap(&values[i],&values[min]);
         }
      }
 }
+
+//Insertion sort
 void insertion(int values[], int n){
     for (int i = 1; i < n; i++){
         int element = values[i];
@@ -51,11 +85,31 @@ void insertion(int values[], int n){
     }
 }
 
+//Bubble Sort
+
+void bubble(int values[], int n){
+    for (int i = 0; i < n; i++){
+        bool sorted = true;
+        for (int j = 0; j < n-1; j++){
+            if (values[j] > values[j+1]){
+                swap(&values[j], &values[j+1]);
+                sorted = false;
+            }
+        }
+        if (sorted){
+            break;
+        }
+    }
+}
+
 /**
  * Sorts array of n values.
  */
 void sort(int values[], int n)
 {
-    insertion(values, n);
+    //Uncomment either of the sorting algorithms to work
+    //selection(values, n);
+    //insertion(values, n);
+    //bubble(values, n);
     return;
 }
